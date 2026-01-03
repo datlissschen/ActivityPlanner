fade93825b7685b0d763f1b08432c276def94c0a
Lisa <lisa.na2710@gmail.com>
2026-01-03 21:24:01 +0100

    add a foto refernce for the webpage

--- src/test/java/com/datlisschen/activityplanner/ActivityPlannerApplicationTests.java
+++ src/test/java/com/datlisschen/activityplanner/ActivityPlannerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ActivityPlannerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
