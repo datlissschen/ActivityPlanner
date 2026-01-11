d331b557e6e8b9ec8927ca593ecb22535a5bc8ad
Lisa <lisa.na2710@gmail.com>
2026-01-11 22:16:26 +0100

    feat: repo querys

--- src/main/java/com/datlisschen/activityplanner/repository/ActivityIdeaRepository.java
+++ src/main/java/com/datlisschen/activityplanner/repository/ActivityIdeaRepository.java
@@ -2,6 +2,8 @@ package com.datlisschen.activityplanner.repository;
 
 import com.datlisschen.activityplanner.model.entity.ActivityIdea;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,10 @@ import java.util.List;
 @Repository
 public interface ActivityIdeaRepository extends JpaRepository<ActivityIdea, Long> {
     List<ActivityIdea> findByExpeditionsId(Long expeditionId);
+
+    @Query("SELECT i FROM ActivityIdea i WHERE " +
+            "LOWER(i.title) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(i.description) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(i.place) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<ActivityIdea> searchIdeas(@Param("query") String query);
 }
--- src/main/java/com/datlisschen/activityplanner/repository/ExpeditionRepository.java
+++ src/main/java/com/datlisschen/activityplanner/repository/ExpeditionRepository.java
@@ -2,8 +2,16 @@ package com.datlisschen.activityplanner.repository;
 
 import com.datlisschen.activityplanner.model.entity.Expedition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-public interface ExpeditionRepository extends JpaRepository<Expedition,Long>{
+import java.util.List;
 
+@Repository
+public interface ExpeditionRepository extends JpaRepository<Expedition, Long> {
+    @Query("SELECT e FROM Expedition e WHERE " +
+            "LOWER(e.name) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(e.expeditionDescription) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Expedition> searchExpeditions(@Param("query") String query);
 }
